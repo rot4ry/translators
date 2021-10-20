953fe5bf6b6c75dc29c1bd91c1788a854ac3dfe8
rot4ry <wojtekw233@gmail.com>
2021-10-20 17:26:34 +0200

    Updated: code cleanup.

--- html-form-validation/script.js
+++ html-form-validation/script.js
@@ -1,27 +1,20 @@
-const createButton = document.querySelector('#frm')
-const submitLock = false
-
-const handleClick = (e) => {
-  e.preventDefault()
-  console.log(this)
-  return true
-}
-
-// check if 
-// - exists?
+// check if:
+// - exists, longer than 0/2
 const validateNames = () => {
-
+  return false
 }
 
+// check if:
+// - is mail address
 const validateMail = () => {
-  
+  return false
 }
 
-// check if phone
+// check if phone:
 // - is long enough
 // - contains numbers
 const validatePhone = () => {
-
+  return false
 }
 
 // check if passwords: 
@@ -29,18 +22,21 @@ const validatePhone = () => {
 // - are equal
 // - longer than X
 const validatePasswords = () => {
-
-}
-
-
-const validateForm = (e) => {
-  validateNames()
-  validateMail()
-  validatePhone()
-  validatePasswords()
-  handleClick(e)
   return false
 }
 
+const validateForm = (e) => {
+  if(!( validateNames() &&
+        validateMail() &&
+        validatePhone() && 
+        validatePasswords())
+  ) {
+    e.preventDefault()
+    console.log('Validation failed.')
+  } else {
+    console.log('Validation succeeded, submitting...')
+  }
+}
 
-createButton.addEventListener('submit', (e) => validateForm(e))
+const formToValidate = document.querySelector('#frm')
+formToValidate.addEventListener('submit', (e) => validateForm(e))
