f71938771893b44e25e5333c950b9dcd806ceb8b
rot4ry <wojtekw233@gmail.com>
2021-10-25 14:15:23 +0200

    Updated: regex tester now handles lack of groups too.

--- regular-expressions/script.js
+++ regular-expressions/script.js
@@ -10,19 +10,25 @@ const validate = () => {
   if(pattern && string) {
     for (const found of string.matchAll(new RegExp(pattern, 'g'))) {
       const newMatch = document.createElement('p')
-      const groups = []
-      for(const [key, value] of Object.entries(found['groups'])) {
-        if(value != null) groups.push(key)
-      }
-
+      let groups = []
+      try {
+        for(const [key, value] of Object.entries(found['groups'])) {
+          if(value != null) groups.push(key)
+        }
+      } catch (e) { groups = null }
+      
       let content = ''
         .concat(`[ `)
         .concat(`match: '${found[0]}', `)
         .concat(`index: ${found['index']}, `)
         .concat(`groups: { `)
-        for (let i = 0; i < groups.length; i++) {
-          content = content.concat(groups[i])
-          if(i < groups.length - 1) content = content.concat(', ')
+        if (groups === null) {
+          content = content.concat('undefined')
+        } else {
+          for (let i = 0; i < groups.length; i++) {
+            content = content.concat(groups[i])
+            if(i < groups.length - 1) content = content.concat(', ')
+          }
         }
         content = content.concat(` } ]`)
 
